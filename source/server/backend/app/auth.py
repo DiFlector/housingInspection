6f6508877756175c79a4a610bca44c786d77a944
root <root@housinginspection.com>
2025-03-03 23:33:35 +0000

    roles

--- source/server/backend/app/auth.py
+++ source/server/backend/app/auth.py
@@ -22,7 +22,7 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
         expire = datetime.utcnow() + expires_delta
     else:
         expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({"exp": expire, "role": data["role"]}) # Добавляем роль!
+    to_encode.update({"exp": expire, "role": data["role"], "user_id": data["user_id"]})  #  ДОБАВИТЬ user_id
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
